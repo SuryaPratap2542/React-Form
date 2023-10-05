c53d79e3e0c4d354a02bce9b29a19af0ad5e822c
sps@#254 <suryapratap2542@gmail.com>
2023-10-05 12:45:05 +0530

    updated form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import './App.css';
 import 'tailwindcss/tailwind.css';
 
-
 function App() {
   const [formData, setFormData] = useState({
     firstName: "",
@@ -14,7 +13,8 @@ function App() {
     notificationType: "Email",
   });
 
-  console.log(formData);
+  const [showPopup, setShowPopup] = useState(false); // State for controlling the popup
+  const [submittedData, setSubmittedData] = useState(null); // State for storing submitted data
 
   function ChangeHandler(event) {
     const { name, value, type } = event.target;
@@ -26,13 +26,22 @@ function App() {
 
   function submitHandler(event) {
     event.preventDefault();
-    console.log("Finallyyyyyyyyyyy.");
-    console.log(formData);
+    // Save the submitted data to display in the popup
+    setSubmittedData(formData);
+    // Show the popup
+    setShowPopup(true);
+  }
+
+  function closePopup() {
+    // Close the popup
+    setShowPopup(false);
   }
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form onSubmit={submitHandler} className="border border-gray-300 shadow-md p-6 rounded-lg">
+    <div className="flex justify-center items-center h-screen bg-sky-200">
+      <form onSubmit={submitHandler} className="border border-gray-300 shadow-md p-6 rounded-lg bg-purple-300 w-200">
+      <h1 className="text-3xl font-bold mb-4">React Form</h1>
+
         <label htmlFor="firstname">First Name</label> <br />
         <input
           type="text"
@@ -109,7 +118,7 @@ function App() {
         />
         <br />
 
-        <fieldset className="border p-2 rounded-lg">
+        <fieldset className="border gap-4 rounded-lg flex flex-row">
           <legend>Notification Type:</legend>
           <input
             type="radio"
@@ -147,6 +156,23 @@ function App() {
           Submit
         </button>
       </form>
+
+      {/* Popup/Modal */}
+      {showPopup && (
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold mb-4">Submitted Data</h2>
+            {/* Display submitted data */}
+            <pre>{JSON.stringify(submittedData, null, 2)}</pre>
+            <button
+              onClick={closePopup}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-4"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
